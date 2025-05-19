a81646d3ed9f9c0208adb39c3d0b23c331d2336f
LUCAGERBER <luca@yobike.fr>
2025-05-19 17:39:30 +0200

    Added new input system

---
+++ Assets/Script/S_Controller.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_Controller : MonoBehaviour
+{
+    private void OnFire()
+    {
+        Debug.Log("azeaze");
+    }
+
+    private void OnPoint()
+    {
+        Debug.Log("point");
+    }
+}
