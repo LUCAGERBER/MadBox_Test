70ae8d884b46ca049e44a1151f28b69c726652cb
LUCAGERBER <luca@yobike.fr>
2025-05-23 23:41:46 +0200

    small tweak to camera death motion

--- Assets/Script/UI/S_LooseScreen.cs
+++ Assets/Script/UI/S_LooseScreen.cs
@@ -33,7 +33,7 @@ public class S_LooseScreen : S_EndScreen
             elapsed += Time.unscaledDeltaTime;
 
             _mainCamera.transform.position += _mainCamera.transform.forward * (_zoomInStrenght * Time.unscaledDeltaTime);
-            _mainCamera.transform.position += -_mainCamera.transform.up * (_zoomInStrenght * Time.unscaledDeltaTime);
+            _mainCamera.transform.position += -_mainCamera.transform.up * (_zoomInStrenght/2 * Time.unscaledDeltaTime);
 
             yield return null;
         }
